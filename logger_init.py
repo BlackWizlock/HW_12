aa447eeb1f9253c89b607465ad39d5296baab40f
BlackWizlock <!JQkSr5a>
2022-05-24 06:54:42 +0300

    Ver 220523

--- logger_init.py
+++ logger_init.py
@@ -1,9 +1,7 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, filemode="w", encoding="utf-8", style="{")
-
 logger_mine = logging.getLogger("logger")
-handler = logging.FileHandler("main_logging.log")
-formatter = logging.Formatter("->>> [%(levelname)s] : [%(asctime)s] : %(message)s")
+handler = logging.FileHandler(filename="main_logging.log", mode="w", encoding="utf-8")
+formatter = logging.Formatter(fmt="->>> [%(levelname)s] : [%(asctime)s] : %(message)s")
 handler.setFormatter(formatter)
 logger_mine.addHandler(handler)
