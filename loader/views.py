dafea60542107aca29fcaf813cfc6db9d94f7f97
BlackWizlock <!JQkSr5a>
2022-05-28 11:18:38 +0300

    Ver. 220528

--- loader/views.py
+++ loader/views.py
@@ -8,9 +8,11 @@ from flask import (
 )
 from main.views import DATABASE
 import os
-import logger_init
+import logging
 
 loader_blueprint = Blueprint("loader_blueprint", __name__, template_folder="templates")
+
+logger_mine = logging.getLogger("logger")
 
 
 @loader_blueprint.route("/post/")
@@ -28,24 +30,24 @@ def uploaded_page(path=None):
     if request.method == "POST":
         picture = request.files.get("picture")
         if not picture:  # Обработка исключения/ошибок
-            logger_init.logger_mine.error("Попытка загрузку неразрешенного типа файла")
+            logger_mine.error("Попытка загрузку неразрешенного типа файла")
             abort(400)
         elif picture.filename == "":
+            logger_mine.info("Попытка загрузку сообщения без файла")
             abort(400)
-            logger_init.logger_mine.info("Попытка загрузку сообщения без файла")
         elif (
-            os.path.splitext(picture.filename)[1].lower()
-            not in current_app.config["UPLOAD_EXTENSIONS"]
+                os.path.splitext(picture.filename)[1].lower()
+                not in current_app.config["UPLOAD_EXTENSIONS"]
         ):
-            logger_init.logger_mine.info("Попытка загрузку неразрешенного типа файла")
+            logger_mine.info("Попытка загрузку неразрешенного типа файла")
             abort(400)
         picture.save(f"./uploads/{picture.filename}")
         text = request.values.get("content")
         DATABASE.json_write(
-            {"pic": "../../uploads/" + picture.filename, "content": text}
+                {"pic": "../../uploads/" + picture.filename, "content": text}
         )
         return render_template(
-            "post_uploaded.html", added_text=text, added_picture=picture.filename
+                "post_uploaded.html", added_text=text, added_picture=picture.filename
         )
     else:
         return send_from_directory("uploads", path)
@@ -58,16 +60,16 @@ def uploaded_page(path=None):
 @loader_blueprint.errorhandler(400)
 def file_type_not_allowed(error):
     return (
-        f"<h1><center><font color='red'>Error with picture file</font>"
-        f"<br>File size are too large."
-        f"<br>Sending message without picture not allowed."
-        f"<br>Only PNG, JPG and GIF are allowed.</center></h1><hr>"
+            f"<h1><center><font color='red'>Error with picture file</font>"
+            f"<br>File size are too large."
+            f"<br>Sending message without picture not allowed."
+            f"<br>Only PNG, JPG and GIF are allowed.</center></h1><hr>"
     )
 
 
 @loader_blueprint.errorhandler(404)
 def page_not_found(error):
     return (
-        f"<h1><center><font color='red'>Error 404</font>"
-        f"<br>Something goes wrong! Page not found.</center></h1><hr>"
+            f"<h1><center><font color='red'>Error 404</font>"
+            f"<br>Something goes wrong! Page not found.</center></h1><hr>"
     )
